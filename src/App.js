391c88d56a804601261a101d0d914ba20e5f565a
Jayant Verma <iamvermajayant@gmail.com>
2022-05-09 11:41:36 +0530

    env added

--- src/App.js
+++ src/App.js
@@ -3,7 +3,8 @@ import axios from "axios";
 import "./App.css";
 import NavBar from "./components/NavBar";
 import NewsComponent from "./components/NewsComponent/NewsComponent";
-import apikey from "./Data/config";
+//import apikey from "./Data/config";
+//eslint-disable-next-line
 import Footer from "./components/Footer/Footer";
 
 function App() {
@@ -23,7 +24,7 @@ function App() {
     try {
       const proxy = 'https://cors-anywhere.herokuapp.com/';
       const news = await axios.get(
-        `${proxy}https://newsapi.org/v2/top-headlines?country=in&apiKey=${apikey}&category=${category}&pageSize=${loadMore}`
+        `${proxy}https://newsapi.org/v2/top-headlines?country=in&apiKey=${process.env.REACT_APP_API_KEY}&category=${category}&pageSize=${loadMore}`
       );
       setNewsArray(news.data.articles);
       setNewsResult(news.data.totalResults);
